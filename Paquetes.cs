94341ecb1466f2279d164f774b14a3ee3c89869f
dogras <dogras_mo20@hotmail.com>
2021-04-19 14:23:33 -0600

    Mega cambiaso del flavio o si o si

--- Paquetes.cs
+++ Paquetes.cs
@@ -20,26 +20,58 @@ namespace Sistema_de_deciciones_de_Funeraria
 
         private void bt_Estandar_Click(object sender, EventArgs e)
         {
-            Datos da = new Datos();
-            da.Show();
+            if(MessageBox.Show("Estandar: Ataúd Metálico, Mobiliario para 50 personas, Traslado C-I-P, 4 sirios. $15,600.00. ¿Desea contratar este paquete?", "Aviso", MessageBoxButtons.YesNo, MessageBoxIcon.Information)== DialogResult.Yes)
+            {
+                Datos da = new Datos();
+                da.Show();
+                this.Hide();
+            }
+            else
+            {
+                bt_Economico.Focus();
+            }
         }
 
         private void bt_Economico_Click(object sender, EventArgs e)
         {
-            Datos da = new Datos();
-            da.Show();
+            if (MessageBox.Show("Economico: Velación en capilla, Flores, Servicio de Cafetería Ilimitado $28,400.00 ¿Desea contratar este paquete?", "Aviso", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+            {
+                Datos da = new Datos();
+                da.Show();
+                this.Hide();
+            }
+            else
+            {
+               bt_Oro.Focus();
+            }
         }
 
         private void bt_Oro_Click(object sender, EventArgs e)
         {
-            Datos da = new Datos();
-            da.Show();
+            if (MessageBox.Show("Oro: Misa de cuerpo presente, Ataúd de Madera, Sirios Ilimitados $65,400.00. ¿Desea contratar este paquete?", "Aviso", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+            {
+                Datos da = new Datos();
+                da.Show();
+                this.Hide();
+            }
+            else
+            {
+                bt_Diamante.Focus();
+            }
         }
 
         private void bt_Diamante_Click(object sender, EventArgs e)
         {
-            Datos da = new Datos();
-            da.Show();
+            if (MessageBox.Show("Diamante: Ataúd es de madera fina o plus (Cedro, Caoba), Cremación.  $105,200.00. ¿Desea contratar este paquete?", "Aviso", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+            {
+                Datos da = new Datos();
+                da.Show();
+                this.Hide();
+            }
+            else
+            {
+                bt_Estandar.Focus();
+            }
         }
     }
 }
